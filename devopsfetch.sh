c7765c0a63ca04ba1e29b7aa2e6ed7af9bab41d5
Rob-in-son <estherchidinma202@gmail.com>
2024-07-23 21:00:25 +0100

    Help function

--- devopsfetch.sh
+++ devopsfetch.sh
@@ -29,7 +29,15 @@
 
 # Help
 help(){
-    free -m | grep Mem
+    echo """Usage: devopsfetch [OPTIONS]
+    Your devops tool for server information retrieval and monitoring
+    Global Options: 
+        -p  --port    Display all active ports and services 
+        -d  --docker  List all Docker images and containers 
+        -n  --nginx   Display all Nginx domains and their ports 
+        -u  --users   List all users and their last login times
+        -t  --time    Display activities within a specified time range
+        -h  --help    Provide usage instructions for the program"""
 }
 
 # Process options when the devopsfetch is called
