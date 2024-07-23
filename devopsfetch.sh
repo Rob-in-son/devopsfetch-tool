3ae762986d6a5581c396416a69215160393f6648
Rob-in-son <estherchidinma202@gmail.com>
2024-07-23 19:18:56 +0100

    First commit

---
+++ devopsfetch.sh
@@ -0,0 +1,57 @@
+#!bin/bash
+
+# Define functions for each information required
+
+# ports
+ports(){
+
+}
+
+# docker
+docker(){
+
+}
+
+# Nginx
+nginx(){
+
+}
+
+# Users
+users(){
+
+}
+
+# Time range
+time_range(){
+
+}
+
+# Help
+help(){
+
+}
+
+# Process options when the devopsfetch is called
+while devopsfetch 'pdnuth' OPTIONS; do 
+    case "$OPTION" in
+        p) 
+            ;;
+
+        d) 
+            ;;
+
+        n) 
+            ;;
+
+        u) 
+            ;;
+
+        t) 
+            ;;
+        
+        h) 
+            ;;
+
+    esac
+done
